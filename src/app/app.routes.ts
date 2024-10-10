9604e927baecc29bda0395cd908c59cb2588939a
zhales05 <zhales05@gmail.com>
2024-10-10 00:22:22 -0600

    added demo button from material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AppointmentsComponent } from './appointments/appointments.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppointmentsComponent } from './components/appointments/appointments.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
